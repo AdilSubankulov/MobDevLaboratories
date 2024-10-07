2404207ba6379cc22d3058667afaf5784c75e4f7
Adil <subankulov_a@iuca.kg>
2024-10-07 15:23:31 +0600

    Task 1

---
+++ app/src/main/java/kg/iuca/MyFirstApp/MainActivity.kt
@@ -0,0 +1,53 @@
+package kg.iuca.MyFirstApp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import kg.iuca.MyFirstApp.ui.theme.MyFirstAppTheme
+import kotlin.random.Random
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyFirstAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Task1()
+                }
+            }
+        }
+    }
+
+    @Composable
+    fun Task1() {
+
+        val helloWorld = remember { mutableStateOf("Привет, мир!") }
+
+        Column {
+            Text(text = helloWorld.value)
+
+            Button(onClick = {
+                helloWorld.value = "Кнопка нажата"
+            }) {
+                Text("Button")
+            }
+
+        }
+
+
+    }
+}
